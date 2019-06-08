b962c1c172e65f823a05bef7e5a5f98fd6aa13d9
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-08 07:57:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :hauntedhouses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.intger :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+    end
+end
